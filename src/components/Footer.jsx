6ad7b712d9990079df4004361c3212a4ea6d6952
vinicius-lima-barbosa <viniciusbarbosa0202@gmail.com>
2024-09-27 14:10:05 -0300

    fix: fixing footer v2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="bg-gray-100 text-gray-600 p-4 mt-8">
       <div className="container mx-auto text-center">
-        <p>&copy; 2024 Sistema de Reviews. Todos os direitos reservados.</p>
+        <p>&copy; 2024 Review Sistem. All rights reserved.</p>
       </div>
     </footer>
   );
